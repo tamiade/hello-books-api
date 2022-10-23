56c0f770baf2eaec3d5a22f1e39383a6c32d45bc
Tami <tamiadetayo@gmail.com>
2022-10-23 11:19:14 -0700

    Created read books endpoint

---
+++ app/routes.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Americanah", "About immigration and becoming"),
+    Book(2, "Half of a yellow sun", "About history and becoming"),
+    Book(3, "Purple Hibiscus", "About abuse and becoming")
+]
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
+
