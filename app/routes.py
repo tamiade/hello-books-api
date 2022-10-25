1b4618324c5d748bec5be2bb03fbe4102e9ea3d5
Tami <tamiadetayo@gmail.com>
2022-10-24 19:17:15 -0700

    Created read one book endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -24,4 +24,26 @@ def handle_books():
             "description": book.description
         })
     return jsonify(books_response)
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message": f"{book_id} invalid"}, 400))
+    
+    for book in books:
+        if book.id == book_id:
+           return book
+
+    abort(make_response({"message": f"{book_id} not found"}, 404))
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
 
