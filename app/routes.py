0fc35f1cc36b16fe006c9fd751937261b558b008
Tami <tamiadetayo@gmail.com>
2022-11-01 02:21:43 -0700

    Setup model and created book records

--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,29 @@
-from flask import Blueprint, jsonify, abort, make_response
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.book import Book 
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+books_bp = Blueprint("books", __name__, url_prefix="/books")  
 
-books = [
-    Book(1, "Americanah", "About immigration and becoming"),
-    Book(2, "Half of a yellow sun", "About history and becoming"),
-    Book(3, "Purple Hibiscus", "About abuse and becoming")
-]
+@books_bp.route("", methods=["POST"])
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"], description=request_body["description"])
 
-books_bp = Blueprint("books", __name__, url_prefix="/books")
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully added", 201)
+
 
 @books_bp.route("", methods=["GET"])
-def handle_books():
+def read_all_books():
     books_response = []
+    books = Book.query.all()
     for book in books:
         books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
     return jsonify(books_response)
-
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        abort(make_response({"message": f"{book_id} invalid"}, 400))
-    
-    for book in books:
-        if book.id == book_id:
-           return book
-
-    abort(make_response({"message": f"{book_id} not found"}, 404))
-
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book = validate_book(book_id)
-
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-    }
 
