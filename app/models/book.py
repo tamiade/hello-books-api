d65204818c19ad965b1033dff8300064146293f1
Tami <tamiadetayo@gmail.com>
2023-03-26 20:15:04 -0700

    Add BookGenre model

--- app/models/book.py
+++ app/models/book.py
@@ -7,6 +7,7 @@ class Book(db.Model):
     description = db.Column(db.String)
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
     author = db.relationship("Author", back_populates="books")
+    genres = db.relationship("Genre", secondary="book_genre", backref="books")
 
     def to_dict(self):
         return {"id": self.id, "title": self.title, "description": self.description}
